08db2cf6dc9cef4f3a828f224c0384b0fc20c990
grantnelsonmd <62182061+grantnelsonmd@users.noreply.github.com>
2025-05-14 15:25:24 -0600

    removed gridlines from homepage (put in cta)

--- frontend/src/components/neo/neobrutal-home.tsx
+++ frontend/src/components/neo/neobrutal-home.tsx
@@ -113,14 +113,6 @@ const NeobrutalHome = () => {
           />
         
           </div>
-
-          {/* gridlines */}
-          {/* <div className="absolute inset-0 z-0" style={{
-            backgroundImage: "linear-gradient(to right, rgba(255,255,255,0.1) 1px, transparent 1px), linear-gradient(to bottom, rgba(255,255,255,0.1) 1px, transparent 1px)",
-            backgroundSize: "40px 40px",
-            transform: "perspective(500px) rotateX(60deg)",
-            transformOrigin: "bottom",
-          }} /> */}
         </div>
       </section>
 
